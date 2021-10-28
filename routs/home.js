9cefe893851354b3f2f0a8706163c267182a438b
BrandoSoft <csbrandox@gmail.com>
2021-10-28 21:04:39 +0200

    party component done.

--- routs/home.js
+++ routs/home.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const {PARTY_INFO} = require("../data/party-info");
+
 const homeRouter = express.Router();
 
 homeRouter
     .get('/', (req, res) =>{
-        res.render( 'home/index')
+        res.render( 'home/index', {
+            info:PARTY_INFO,
+        })
     });
 
 module.exports = {
