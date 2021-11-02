988d962ef988ce926dd11ac8c84ba4fb896165e9
Jutrzenka <kacperczaja1999@gmail.com>
2021-11-02 15:31:04 +0100

    Przywrócenie env

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const config = {
-    port: Number(process.argv[3]) || 3000,
-    database_name: "twojaimpreza",
-    database_pass: process.argv[2] || "KyhgX8Rcu795aVp",
+    port: Number(process.env.PORT) || 3000,
+    database_name: process.env.BASENAME,
+    database_pass: process.env.BASEPASS,
 }
 
 module.exports = config;
