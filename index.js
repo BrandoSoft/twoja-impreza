6238628e9174c39d608506af7834b9c2610c7c56
Jutrzenka <kacperczaja1999@gmail.com>
2021-11-03 21:34:29 +0100

    Routingi 2

--- index.js
+++ index.js
@@ -7,26 +7,22 @@ const {homeRouter,} = require("./routs/home");
 const {eventsRouter,} = require("./routs/add");
 const {archiveRouter,} = require("./routs/archive");
 const {userRouter,} = require("./routs/user-account");
-
-
 const {port} = require('./config')
 
-// Routing
 const app = express();
-app.use(express.static('./public'));
-app.use('/events', eventsRouter);
-app.use('/archive', archiveRouter);
-app.use('/user', userRouter);
-app.use('/', homeRouter);
 
 // handlebars config
 app.engine('.hbs', hbs({extname: 'hbs', helpers: handlebarsHelpers}));
 app.set('view engine', '.hbs');
 
+// Routing
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}))
-
 app.use(bodyParser.json())
-
+app.use('/events', eventsRouter);
+app.use('/archive', archiveRouter);
+app.use('/user', userRouter);
+app.use('/', homeRouter);
 
 app.listen(port, () => {
     console.log(`Working on http://localhost:${port}`)
