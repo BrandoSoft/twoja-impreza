5472fe193ae017b6d5bf1b962530b401f2dc7fe5
BrandoSoft <csbrandox@gmail.com>
2021-11-03 21:37:09 +0100

    more info in party component worsk

--- routs/home.js
+++ routs/home.js
@@ -6,20 +6,30 @@ const homeRouter = express.Router();
 const {handlebarsHelpers} = require('../utils/handlebars-helpers')
 
 homeRouter
-    .get('/', (req, res) =>{
+    .get('/', (req, res) => {
 
         PartyList.find().lean()
             .then(data => {
 
                 console.log(data)
 
-                res.render( 'sites/home/home', {
-                    info:data,
+                res.render('sites/home/home', {
+                    info: data,
                 })
             })
             .catch(error => console.log(error))
 
-    });
+    })
+    .get('/party/:id', (req, res) => {
+        PartyList.findOne({
+            _id: req.params.id,
+        })
+            .then(data =>{
+                res.render('sites/home/party-info', {
+                    data:data
+                })
+            })
+    })
 
 module.exports = {
     homeRouter,
